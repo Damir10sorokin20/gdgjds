ae45a54ceb967e52c08ca10eb75301bbbadb75ea
Damir10sorokin20 <114170327+Damir10sorokin20@users.noreply.github.com>
2023-05-14 17:40:58 +0300

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+const float WINDOW_WIDTH = 480;
+const float WINDOW_HEIGHT = 320;
+
+
+const std::string IMAGES_FOLDER = "images\\";
+const std::string TILES_FILE_NAME = "images\\tiles.png";
+const std::string BACK_FILE_NAME = "images\\background.png";
+const std::string FRAME_FILE_NAME = "images\\frame.png";
+const std::string FONT_FILE_NAME = "ds-digib.ttf";
+const int CHAR_SIZE = 48;
